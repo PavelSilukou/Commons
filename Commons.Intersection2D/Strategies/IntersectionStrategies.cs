54e829e9c2fc877d66f04dde55aaf5102d9fef53
Pavel <p.silukov@outlook.com>
2025-08-13 19:53:13 +0300

    Refactoring

--- Commons.Intersection2D/Strategies/IntersectionStrategies.cs
+++ Commons.Intersection2D/Strategies/IntersectionStrategies.cs
@@ -12,9 +12,17 @@ namespace Commons.Intersection2D.Strategies
 		
 		internal IIntersectionStrategy GetStrategy(CShape shape1, CShape shape2)
 		{
-			var strategy = GetStrategyByShapes(shape1, shape2);
+			var pair = new IntersectionShapeTypesPair(shape1.GetType(), shape2.GetType());
+			var strategy = _strategies.GetValueOrDefault(pair);
 			if (strategy != null) return strategy;
-			throw new IntersectionStrategyException($"There is no strategy for intersection {shape1.GetType().Name} and {shape2.GetType().Name}.");
+			
+			strategy = GetStrategyByShapes(shape1, shape2);
+			if (strategy == null) {
+				throw new IntersectionStrategyException($"There is no strategy for intersection {shape1.GetType().Name} and {shape2.GetType().Name}.");
+			}
+			
+			_strategies.Add(pair, strategy);
+			return strategy;
 		}
 
 		private static IEnumerable<IIntersectionStrategy> FindAllStrategies()
